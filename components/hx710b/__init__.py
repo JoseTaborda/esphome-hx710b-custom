b88548ca7adedee7da6b9dbd9f665e5665b24d99
JoseTaborda <taborda@mail.telepac.pt>
2025-08-08 16:45:12 +0100

    Correção completa do schema YAML

--- components/hx710b/__init__.py
+++ components/hx710b/__init__.py
@@ -1,11 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_DOUT_PIN, CONF_CLK_PIN
 
 DEPENDENCIES = []
 CODEOWNERS = ["@JoseTaborda"]
-CONF_HX710B = "hx710b"
 
 hx710b_ns = cg.esphome_ns.namespace('hx710b')
 HX710BSensor = hx710b_ns.class_('HX710BSensor', sensor.Sensor, cg.PollingComponent)
@@ -15,6 +14,6 @@ CONFIG_SCHEMA = sensor.sensor_schema(
     accuracy_decimals=2,
     unit_of_measurement="kg"
 ).extend({
-    cv.Required('dout_pin'): cv.use_id(cg.GPIOPin),
-    cv.Required('clk_pin'): cv.use_id(cg.GPIOPin),
-})
+    cv.Required(CONF_DOUT_PIN): cv.use_id(cg.GPIOPin),
+    cv.Required(CONF_CLK_PIN): cv.use_id(cg.GPIOPin),
+}).extend(cv.polling_component_schema('20s'))
--- components/hx710b/sensor.py
+++ components/hx710b/sensor.py
@@ -1,14 +1,13 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome.components import sensor
-from . import HX710BSensor, CONF_HX710B
+from . import CONF_HX710B, HX710BSensor
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
     
-    dout_pin = await cg.gpio_pin_expression(config['dout_pin'])
-    clk_pin = await cg.gpio_pin_expression(config['clk_pin'])
-    cg.add(var.set_dout_pin(dout_pin))
-    cg.add(var.set_clk_pin(clk_pin))
+    dout = await cg.gpio_pin_expression(config[CONF_DOUT_PIN])
+    clk = await cg.gpio_pin_expression(config[CONF_CLK_PIN])
+    cg.add(var.set_dout_pin(dout))
+    cg.add(var.set_clk_pin(clk))
