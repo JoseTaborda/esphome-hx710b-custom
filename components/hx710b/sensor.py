1c0af8446d9342ef208c61d6e5d350a400dba000
JoseTaborda <taborda@mail.telepac.pt>
2025-08-08 12:45:12 +0100

    Corrige imports no sensor.py

--- components/hx710b/sensor.py
+++ components/hx710b/sensor.py
@@ -1,14 +1,12 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID
 
-hx710b_ns = cg.esphome_ns.namespace("hx710b")
+# Adicione estas linhas:
+hx710b_ns = cg.esphome_ns.namespace('hx710b')
+HX710BSensor = hx710b_ns.class_('HX710BSensor', sensor.Sensor, cg.PollingComponent)
 
 CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(HX710BSensor),
 })
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await sensor.register_sensor(var, config)
